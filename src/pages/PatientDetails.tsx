7907e86f13839480489ecfa82514eae5c6f5e8cb
André <andre4094@gmail.com>
2025-07-25 00:40:17 +0300

    Update PatientDetails.tsx

--- src/pages/PatientDetails.tsx
+++ src/pages/PatientDetails.tsx
@@ -175,7 +175,7 @@ export default function PatientDetails() {
   if (loading) {
     return (
       <SidebarProvider>
-        <div className="flex h-screen">
+        <div className="flex h-screen w-full">
           <AppSidebar />
           <SidebarInset>
             <AppHeader />
