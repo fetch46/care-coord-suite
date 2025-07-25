1ac721dadeb1ca64de38e88208723a0632f98bb1
André <andre4094@gmail.com>
2025-07-25 13:06:05 +0300

    Update app-header.tsx

--- src/components/ui/app-header.tsx
+++ src/components/ui/app-header.tsx
@@ -19,7 +19,7 @@ export function AppHeader() {
         <SidebarTrigger />
         
         {/* Search */}
-        <div className="flex-1 max-w-md">
+        <div className="flex-1 max-w-full">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
             <Input
