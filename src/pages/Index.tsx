a62d5bd9e63459c551c51d0d19e06738ae46506b
André <andre4094@gmail.com>
2025-07-28 16:50:27 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -112,6 +112,16 @@ const Index = () => {
                 <RecentActivity />
               </div>
             </div>
+            
+            <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-6">
+              <div className="lg:col-span-1">
+                <QuickActions />
+              </div>
+              <div className="lg:col-span-2">
+                <RecentActivity />
+              </div>
+            </div>
+
 
             {/* Additional Cards */}
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-6">
