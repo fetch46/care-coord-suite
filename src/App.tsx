2b5e09d1ce7a00a2650ec2bc3c0349739fc57f21
André <andre4094@gmail.com>
2025-07-29 14:15:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Patients from "./pages/Patients";
 import Schedule from "./pages/Schedule";
-import StaffDetails from "./pages/ScheduleDetails";
+import ScheduleDetails from "./pages/ScheduleDetails";
 import PatientDetails from "./pages/PatientDetails";
 import Staff from "./pages/Staff";
 import StaffDetails from "./pages/StaffDetails";
